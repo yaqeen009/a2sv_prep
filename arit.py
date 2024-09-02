d6711fe002d4b941f286b5e1ce1de729d2a5b542
Yaqeen Mumuni <yaqeens@Yaqeens-Mac.local>
2024-09-02 19:25:30 +0000

    First problem

---
+++ arit.py
@@ -0,0 +1,9 @@
+if __name__ == '__main__':
+    a = int(input("Enter the value for a: "))
+    b = int(input("Enter the value for b: "))
+    l1 = a + b
+    l2 = a - b
+    l3 = a * b
+    print(f'The sum of {a} and {b} is {l1}')
+    print(f'The difference between {a} and {b} is {l2}')
+    print(f'The product of {a} and {b} is {l3}')
